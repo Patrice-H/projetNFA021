43f486082396dae388b038205e7134642cb955e9
Patrice-H <https://github.com/>
2019-03-13 01:43:35 +0100

    Script du premier atelier

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,37 @@
 ﻿
+function verifChamps(ch){
+    if(ch){
+       return true; 
+    }
+    else{
+        return false;
+    }
+}
+
+function envoieForm(){  
+    var validation;
+    var formulaire = {};
+    formulaire.nom = document.getElementsByName("nom")[0].value;
+    formulaire.prenom = document.getElementsByName("prenom")[0].value;
+    formulaire.fonction = document.getElementsByName("fonction")[0].value;
+    formulaire.mail = document.getElementsByName("mail")[0].value;
+    formulaire.naissance = document.getElementsByName("naissance")[0].value;
+    formulaire.pass = document.getElementsByName("pass")[0].value;
+    formulaire.repass = document.getElementsByName("repass")[0].value;
+    
+    for(var i in formulaire){
+        
+        if(!verifChamps(formulaire[i])){            // a revoir (<input required>)
+            alert("Le champs " + i + " doit être renseigné");
+            validation = false;
+            break;
+        }
+        else{
+            validation = true;
+        }
+    }
+    
+    return validation;
+    
+    
+}
