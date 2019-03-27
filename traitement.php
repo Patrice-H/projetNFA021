54e2599c69403ff1fa1970aba93615306b3034dc
Patrice-H <https://github.com/>
2019-03-27 18:34:58 +0100

    a revoir ave le javascript

--- traitement.php
+++ traitement.php
@@ -1,14 +1,20 @@
 <?php
-    
-    $nom = $_POST['nom'];
+	header('content-type: text/html; charset=utf-8');
+	
+	/*if(isset($_POST['nom'])){
+		$nom = "essai";
+	}
+	else {
+		$nom = $_POST['nom'];
+	}*/
+   /* $nom = $_POST['nom'];
     $prenom = $_POST['prenom'];
-    $fonction = $_POST['fonction'];
+    $fonction = $_POST['fonction'];*/
     $mail = $_POST['mail'];
-    $naissance = $_POST['naissance'];
-    $pass = $_POST['pass'];
-    $repass = $_POST['repass'];
+   /* $enregistrement = $_POST['enregistrement'];
+    $pass = $_POST['pass'];*/
+    $ecriture = true;
     
-    echo $nom . " " . $prenom . " " . $fonction;
     try{
         $bdd = new PDO('mysql:host=localhost;dbname=projetNFA021;charset=utf8', 'root', '');
     }
@@ -16,4 +22,38 @@
         die($e->getMessage());
     }
     
+    $donnees = $bdd->query('SELECT * FROM client');
+    while ($champ = $donnees->fetch()){
+
+    	if($champ['mail'] === $mail){
+    		
+    		$ecriture = false;
+    		echo "true";
+    		
+    		/*$doc = new DOMDocument();
+    		$doc->loadHTMLFile(utf8_decode("HTML/inscription.html"));
+    		$trans = utf8_encode("Cette adresse e-mail est déjà utilisée");
+    		$text = $doc->createTextNode($trans);
+    		$span = $doc->getElementById('erreur_mail');	//->firstChild();
+    		$var = $span->firstChild();
+    		
+    		$span->appendChild($text);
+    		$doc->saveHTMLFile("HTML/inscription.html");	
+            header('Location: HTML/inscription.html');*/
+           	//exit();
+        }
+    }
+        
+	if($ecriture){
+		$req = $bdd->prepare('INSERT INTO client(nom, prenom, fonction, mail, date_enregistrement, pass) VALUES(:nom, :prenom, :fonction, :mail, :date_enregistrement, :pass)');
+		$req->execute(array(
+			'nom' => $_POST['nom'],
+			'prenom' => $_POST['prenom'],
+			'fonction' => $_POST['fonction'],
+			'mail' => $_POST['mail'],
+			'date_enregistrement' => $_POST['enregistrement'],
+			'pass' => $_POST['pass']	
+        	));
+        	echo utf8_encode("Votre compte est enregistré");
+ 	}   
 ?>
