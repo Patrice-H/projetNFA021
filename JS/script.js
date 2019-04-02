dd6bf64e1c3e2b0eae43660b518a717cafa10806
Patrice-H <https://github.com/>
2019-04-02 17:55:00 +0200

    Javascript du second atelier

--- JS/script.js
+++ JS/script.js
@@ -2,33 +2,25 @@
  *                                                                  *
  *  Script de l'exercice sur le formulaire d'inscription 			*
  *                                                                  *
- *  @ C:/wamp64/www/projetNFA021/HTML/inscription.html              */
+ *  @see ../projetNFA021/HTML/inscription.html                      *
+ ********************************************************************/
 
-/*function conversion(chaine){
-	var rep = "";
-	for(i = 0; i < chaine.length; i++){
-		if(isNaN(chaine[i])){
-			rep += ",";
-		}
-		else{
-			rep += chaine[i];
-		}
-	}
-	return rep;
-}*/
 
-function verifText(chaine){
-	var rep = true;
-	for(i = 0; i < chaine.length; i++){
-		if(!isNaN(parseInt(chaine[i], 10))){
-			rep = false;
-		}
-	}
-	return rep;
-}
+var formulaire = {};
 
+/**
+ * Instancie et retourne un objet de type XMLHttpRequest -
+ * 
+ * Renvoie false sinon
+ * @throw ActiveX XMLHttp < Internet Explorer 7 : e1
+ * @throw ActiveX XMLHttp >= Internet Explorer 7 : e2
+ * @throw XMLHttpRequest autres navigateurs : e3
+ * @returns {Object\Boolean} : xhr
+ */
 function getXhr(){
+	
 	var xhr = null;
+	
 	try {
 		xhr = new ActiveXObject('Msxml2.XMLHTTP');
 	}
@@ -49,48 +41,116 @@ function getXhr(){
 	return xhr;
 }
 
+/**
+ * Vérifie la concordance des mots de passes saisis -
+ * 
+ * Modifie le DOM Id = 'erreur_repass'
+ * @param {String} : pass
+ * @param {String} : repass
+ */
+function controleDoubleSaisie(pass, repass){
+	
+	if(pass !== repass){
+    	document.getElementById("erreur_repass").innerHTML  = "Les mots de passe ne sont pas identiques";
+    	return false;
+    }
+    else{
+    	document.getElementById("erreur_repass").innerHTML  = "";
+    	return true;
+    }
+}
+
+/**
+ * Vérifie la longueur du mot de passe passé en paramètre -
+ * 
+ * Modifie le DOM Id = "erreur_pass"
+ * @param {String} : pass
+ * @returns {Boolean}
+ */
+function controleLongueur(pass){
+	
+	if(pass.length < 8){
+		document.getElementById("erreur_pass").innerHTML  = "votre mot de passe doit contenir 8 caratères au moins";
+		return false;
+	}
+	else{
+		document.getElementById("erreur_pass").innerHTML  = "";
+		return true;
+	}
+}
+
+/**
+ * Effectue la checklist des contrôles -
+ * 
+ * valide le formulaire
+ * @param responseText Ajax : reponse
+ */
+function controleValidation(reponse){
+	
+	var validation = true;
+	
+	if(reponse == "false"){
+		validation = false;
+	}
+	if(!controleLongueur(formulaire.pass)){
+		validation = false;
+	}
+	if(!controleDoubleSaisie(formulaire.pass, formulaire.repass)){
+		validation = false;
+	}
+	if(validation){
+		document.getElementById("saisie").submit();
+	}
+}
+
+/**
+ * Vérifie la réponse texte Ajax fournie en paramètre -
+ * 
+ * Modifie le DOM Id = "erreur_mail"
+ * @param responseText Ajax : resultat
+ */
+function controleMail(resultat){
+	
+	if(resultat == "false"){
+		document.getElementById("erreur_mail").innerHTML  = "Votre e-mail est déjà enregistré";
+	}
+	else{
+		document.getElementById("erreur_mail").innerHTML  = "";
+	}
+	controleValidation(resultat);
+}
+
+
+/**
+ * Requête Ajax vérifiant si un e-mail est déjà enregistré -
+ * 
+ * Renvoie la reponse en paramètre de la fonction validMail()
+ * @param {String} : data
+ */
 function ajaxReq(data){
-	var recup;
-	var chargement = "vérification en cours"
+	
 	var xhr = getXhr();
 	    
 	xhr.onreadystatechange = function(){
 		if (xhr.readyState == 4){
 			if (xhr.status == 200){
-	    		validation = xhr.responseText;
-	    		
-	    		//if(validation == "false"){
-	    			document.getElementById("erreur_mail").innerHTML  = "Votre e-mail est déjà enregistré";
-	    		//}
-	    		
-	    		
-	    		//return validation;
+	    		controleMail(xhr.responseText);	
 			}
-	    	//else{
-	    		//alert("status = " + xhr.status);
-	    	//}
 		}
-		else{
-	    	alert("readyState = " + xhr.readyState);
-	    	//if(xhr.readyState < 4){
-	    	//	document.getElementById("erreur_mail").innerHTML  = chargement += ".";
-	    	//}
-	    }
 	};
 	xhr.open("POST", "../traitement.php", true);
 	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	xhr.send("mail="+data);
-	
-	
-	//if(xhr.readyState == 4 && xhr.status == 200){
-	//	alert("val " + validation);
-	//}
 }
 
-
-function envoieForm(){  
-    validation = false;
-    var formulaire = {};
+/**
+ * Fonction d'entrée du script déclenchée à l'évènement onsubmit du formulaire -
+ * 
+ * Encapsule les données dans un objet formulaire et déclenche les contôles de saisie
+ * @returns {Boolean}
+ */
+function envoieForm(){
+	
     formulaire.nom = document.getElementsByName("nom")[0].value;
     formulaire.prenom = document.getElementsByName("prenom")[0].value;
     formulaire.fonction = document.getElementsByName("fonction")[0].value;
@@ -99,88 +159,6 @@ function envoieForm(){
     formulaire.pass = document.getElementsByName("pass")[0].value;
     formulaire.repass = document.getElementsByName("repass")[0].value;
     
-    // Vérification de la double saisie du mot de passe
-    
-    if(formulaire.pass !== formulaire.repass){
-    	document.getElementById("erreur_repass").innerHTML  = "Les mots de passe ne sont pas identiques";
-    	validation = false;
-    }
-    else{
-    	document.getElementById("erreur_repass").innerHTML  = "";
-    	
-    }
-    
-    // Vérification de la validité de la date d'enregistrement (=== date du jour)
-   
-    var dateJour = new Date();
-    var dateTable = formulaire.date.split("-");
-    var dateEnregistrement = new Date(dateTable[0],dateTable[1] - 1,dateTable[2]);
-    
-    if(dateJour.getDate() !== dateEnregistrement.getDate() || dateJour.getMonth() !== dateEnregistrement.getMonth() || dateJour.getFullYear() !== dateEnregistrement.getFullYear()){
-    	
-    	document.getElementById("erreur_enregistrement").innerHTML  = "La date indiquée doit être celle d'aujourdhui";
-    	validation = false;
-    }
-    else{
-    	document.getElementById("erreur_enregistrement").innerHTML  = "";
-    	
-    }
-    
-    // Vérification de la conformité de l'adresse e-mail
-
     ajaxReq(formulaire.mail);
-    
-   // if(!ajaxReq(formulaire.mail)){
-   // 	alert("validation = " + validation + " - ");
-   // }
-   // if(typeof(ajaxReq(formulaire.mail)) == "undefined"){validation = false;}
-    
-    // Vérification de la conformité du type des champs 'nom' et 'prenom' (!= NaN)
-    
-    if(!verifText(formulaire.nom)){
-    	document.getElementById("erreur_nom").innerHTML  = "Votre nom ne doit comporter que des lettres";
-    	validation = false;
-    }
-    else{
-    	document.getElementById("erreur_nom").innerHTML  = "";
-    	
-    }
-    if(!verifText(formulaire.prenom)){
-    	document.getElementById("erreur_prenom").innerHTML  = "Votre prénom ne doit comporter que des lettres"
-    	validation = false;
-    }
-    else{
-    	document.getElementById("erreur_prenom").innerHTML  = ""
-        	
-    }
-    
-    // Vérification de la conformité du mot de passe (longueur, caractères obligatoires)
-   
- 
-    // requète AJAX
-    
-    /*var recup;
-    var xhr = getXhr();
-	    
-	xhr.onreadystatechange = function(){
-		if (xhr.readyState == 4){
-			if (xhr.status == 200){
-	    		recup = xhr.responseText;
-	    		alert("recup1 = " +  recup);
-	    		//return recup;
-			}
-	    	//else{
-	    		//alert("status = " + xhr.status);
-	    	//}
-		}
-	    else{
-	    		alert("readyState = " + xhr.readyState);
-	    }
-	};
-	xhr.open("POST", "../verifMail.php", true);
-	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	xhr.send("mail="+data);*/
-	
-	
-    return validation;       
+    return false;       
 }
